24883f2cc4046c34cb6fae8a993e90389082c9c8
Delton Ding <dsh0416@gmail.com>
2018-11-19 00:04:36 +0900

    Minor fixesx

--- server.rb
+++ server.rb
@@ -2,8 +2,9 @@ require 'bundler'
 Bundler.require
 require 'logger'
 
-Dir[File.dirname(__FILE__) + '/handlers/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/routes/*.rb'].each { |file| require file }
+require '/handlers/runtime_error.db'
+Dir[File.dirname(__FILE__) + '/handlers/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/initializers/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/services/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each { |file| require file }
