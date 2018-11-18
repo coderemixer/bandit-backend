557e83f9a6d7a3dceedd8555951a2a4120ec8192
Delton Ding <dsh0416@gmail.com>
2018-11-19 00:05:06 +0900

    Fix typo

--- server.rb
+++ server.rb
@@ -3,7 +3,7 @@ Bundler.require
 require 'logger'
 
 Dir[File.dirname(__FILE__) + '/routes/*.rb'].each { |file| require file }
-require '/handlers/runtime_error.db'
+require '/handlers/runtime_error'
 Dir[File.dirname(__FILE__) + '/handlers/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/initializers/*.rb'].each { |file| require file }
 Dir[File.dirname(__FILE__) + '/services/*.rb'].each { |file| require file }
